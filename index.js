39feb6519fe341fa5db9d2f2e4278fcdd78439b2
Marco Andronaco <andronacomarco@gmail.com>
2022-09-14 22:07:31 +0200

    add random rich presence

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, ActivityType, PresenceUpdateStatus, Presence } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
@@ -15,7 +15,24 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+function getRandomInt(max) { // not inclusive
+	return Math.floor(Math.random() * (max));
+  }
+
+const activities = [
+	{ activity: ActivityType.Watching, subject: 'i cantieri'}, // sta guardando i cantieri
+	{ activity: ActivityType.Playing, subject: 'briscola'}, // sta giocando a briscola
+	{ activity: ActivityType.Competing, subject: 'prato fiorito'}, // in competizione su prato fiorito
+
+	{ activity: ActivityType.Watching, subject: 'i bambini'},
+	{ activity: ActivityType.Playing, subject: 'quel gioco là non mi viene il titolo'},
+	{ activity: ActivityType.Competing, subject: 'cacata ranked al cesso pubblico'},
+]
+
 client.once('ready', () => {
+	choice = activities[getRandomInt(activities.length)]
+	
+	client.user.setActivity(choice.subject, { type: choice.activity })
 	console.log('Bot online!');
 });
 
